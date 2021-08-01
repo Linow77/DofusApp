1ac4c324833d45b0aa0adeae0c7aec125325d810
Linow <cloos.richard@gmail.com>
2021-08-02 00:54:13 +0200

    Add create task, add fake task

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+  Button,
+  TextInput,
+} from "react-native";
+
+const { StatusBarManager } = NativeModules;
+const STATUSBAR_HEIGHT = Platform.OS === "ios" ? 20 : StatusBarManager.HEIGHT;
 
 export default function App() {
+  const [task, setTask] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const onChangeInput = (text) => {
+    setTask(text);
+  };
+  const createTask = () => {
+    setTasks([...tasks, task]);
+    setTask(null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>test</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Naissances</Text>
+      {tasks.map((item, index) => {
+        return <Text key={index}>{item}</Text>;
+      })}
+      <View style={styles.addTaskSection}>
+        <TextInput
+          style={styles.addInput}
+          onChangeText={(text) => onChangeInput(text)}
+          value={task}
+        />
+        <Button
+          style={styles.addButton}
+          title="+"
+          onPress={() => createTask()}
+        ></Button>
+      </View>
     </View>
   );
 }
@@ -16,6 +50,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: StatusBarManager.HEIGHT,
+    paddingTop: 10,
+    margin: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  addTaskSection: {
+    flexDirection: "row",
+    position: "absolute",
+    justifyContent: "space-around",
+    bottom: 30,
+    width: "100%",
+  },
+  addInput: {
+    backgroundColor: "lightgrey",
+    width: "70%",
+  },
+  addButton: {
+    fontSize: 25,
   },
 });
