5eca54c82e6acb97e80f1e0239d71cbad3fc995b
Linow <cloos.richard@gmail.com>
2021-09-08 23:18:05 +0200

    Ajout d'un menu pour se connecter

--- screen/Home.js
+++ screen/Home.js
@@ -1,45 +1,131 @@
-import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Alert,
+  TextInput,
+} from "react-native";
 
 import { useHistory } from "react-router-native";
 
 const Home = () => {
-  const data = require("../data/user.json");
+  const data = require("../data/admin.json");
   const users = data.users;
   const history = useHistory();
 
-  console.log(users);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [text, setText] = useState("");
+
+  const checkPassword = (password) => {
+    if (password === "7931") {
+      history.push("/inProgress");
+    } else {
+      alert("T'as pas le droit de venir ici :p");
+    }
+  };
   return (
     <View style={styles.screen}>
-      <Text>Selection du compte</Text>
-      {users.map((item, index) => {
-        return (
-          <TouchableOpacity
-            key={index}
-            style={styles.button}
-            onPress={() => history.push("/inProgress")}
-          >
-            <Text style={styles.text}>{item}</Text>
-          </TouchableOpacity>
-        );
-      })}
+      <View style={styles.buttonArea}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.text}>Administrateur</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => history.push("/inProgress")}
+        >
+          <Text style={styles.text}>Invité</Text>
+        </TouchableOpacity>
+      </View>
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modal}>
+          <View style={styles.modalView}>
+            <Text style={styles.text}>Mot de passe</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setText(text)}
+              value={text}
+              keyboardType="numeric"
+            />
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => checkPassword(text)}
+            >
+              <Text style={styles.textStyle}>Valider</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Retour</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    flexGrow: 1,
+    display: "flex",
     alignItems: "center",
+    justifyContent: "center",
     width: "100%",
+    flexGrow: 1,
+  },
+  buttonArea: {
+    height: 150,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: "white",
+    backgroundColor: "#F6F6F6",
+    padding: 10,
+    borderRadius: 10,
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#393E41",
+  },
+  modal: {
+    alignSelf: "center",
+    justifyContent: "center",
+    flexGrow: 1,
+  },
+  modalView: {
+    width: 300,
+
+    display: "flex",
+
+    alignItems: "center",
+    backgroundColor: "#E8EAED",
+    borderWidth: 5,
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    width: "30%",
+    fontSize: 15,
+  },
+  modalButton: {
+    backgroundColor: "#F6F6F6",
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 10,
   },
 });
 export default Home;
